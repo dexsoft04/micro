e294f5963064bcaef2712affa5e122ba56391eb6
wolfplus <wolfplus@gmail.com>
2020-11-06 12:51:04 +0800

    add

--- cmd/platform/kubernetes/installsrv.sh
+++ cmd/platform/kubernetes/installsrv.sh
@@ -6,8 +6,7 @@ if [ $MICRO_ENV == "dev" ]; then
 fi
 
 if [ $MICRO_ENV == "dev" ]; then
-  sed -i '' 's/ghcr.io\/m3o\/platform/ccr.ccs.tencentyun.com\/wolfplus\/mcbeam/g' 'ls ./service'
-  sed -i '' 's/m3o.com/mcbeam.dev/g' 'ls ./service'
-fi
+  sed -i '' 's/ghcr.io\/m3o\/platform/ccr.ccs.tencentyun.com\/wolfplus\/mcbeam/g'
+  sed -i '' '/containers:/i\\      imagePullSecrets:\n      - name: qcloudregistrykey'
 
 kubectl apply -f service
