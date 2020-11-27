a692aa6e318239878a6d5c3e579954f74a3db7c3
wolfplus <wolfplus@gmail.com>
2020-11-27 16:18:55 +0800

    add

--- service/build/golang/bak/golang_bak.go
+++ service/build/golang/bak/golang_bak.go
@@ -1,4 +1,4 @@
-package golang
+package bak
 
 import (
 	"bytes"
@@ -66,17 +66,15 @@ func (g *golang) Build(src io.Reader, opts ...build.Option) (io.Reader, error) {
 
 	// check for vendor directory before setting mod to vendor. the vendor directory wasn't uploaded
 	// in early v3 betas so this enables backwards compatability
-	//args := []string{"build", "-o", "micro_build"}
-	//if _, err := os.Stat(filepath.Join(dir, "vendor")); err == nil {
-	//	args = append(args, "-mod", "vendor")
-	//}
-	//
-	//// build the binary
-	//cmd := exec.Command(g.cmdPath, append(args, ".")...)
-	//cmd.Env = append(os.Environ(), "GO111MODULE=auto")
-	//cmd.Env = append(os.Environ(), "GOPROXY=https://goproxy.io,direct")
+	args := []string{"build", "-o", "micro_build"}
+	if _, err := os.Stat(filepath.Join(dir, "vendor")); err == nil {
+		args = append(args, "-mod", "vendor")
+	}
 
-	cmd := exec.Command("make build")
+	// build the binary
+	cmd := exec.Command(g.cmdPath, append(args, ".")...)
+	cmd.Env = append(os.Environ(), "GO111MODULE=auto")
+	cmd.Env = append(os.Environ(), "GOPROXY=https://goproxy.io,direct")
 
 	cmd.Dir = filepath.Join(dir, options.Entrypoint)
 
