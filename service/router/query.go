d78f57ea83943f89f79c380c8ae098357a83a140
wolfplus <wolfplus@gmail.com>
2020-12-01 15:28:30 +0800

    add

--- service/router/query.go
+++ service/router/query.go
@@ -14,8 +14,6 @@
 
 package router
 
-import "github.com/micro/micro/v3/service/logger"
-
 // LookupOption sets routing table query options
 type LookupOption func(*LookupOptions)
 
@@ -141,14 +139,11 @@ func Filter(routes []Route, opts LookupOptions) []Route {
 
 	// routeMap stores the routes we're going to advertise
 	routeMap := make(map[string][]Route)
-	logger.Debugf("find route uid:%s, address:%s, gateway:%s, network:%s, router:%s, link:%s", uid, address, gateway, network, rtr, link)
 	for _, route := range routes {
-		logger.Debugf("route:%v", route)
 		if isMatch(route, uid, address, gateway, network, rtr, link) {
 			// add matchihg route to the routeMap
 			routeKey := route.Service + "@" + route.Network
 			routeMap[routeKey] = append(routeMap[routeKey], route)
-			logger.Debugf("ismatch true:%v", route)
 
 		}
 	}
