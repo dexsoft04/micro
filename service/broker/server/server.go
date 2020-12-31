029784f1e93f965e3801ff444e90faa391977e5f
wolfplus <wolfplus@gmail.com>
2020-12-31 14:42:17 +0800

    add

--- service/broker/server/server.go
+++ service/broker/server/server.go
@@ -80,7 +80,7 @@ func (h *handler) Publish(ctx context.Context, req *pb.PublishRequest, rsp *pb.E
 	}
 	ns := acc.Issuer
 	subns, ok := metadata.Get(ctx, "Micro-Sub-Namespace")
-	if !ok && len(subns) > 0{
+	if ok && len(subns) > 0{
 		ns = subns
 		logger.Infof("sub namespace:%s", ns)
 	}
