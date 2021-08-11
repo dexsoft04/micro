2c35a39c481d321d73f541e1361b8a35546a49bf
wolfplus <wolfplus@gmail.com>
2021-08-11 16:33:27 +0800

    add

--- cmd/platform/kubernetes/installsrv.sh
+++ cmd/platform/kubernetes/installsrv.sh
@@ -6,14 +6,12 @@ if [ $MICRO_ENV == "dev" ]; then
 fi
 
 if [ $MICRO_ENV == "dev" ]; then
-  sed -i '' 's/ccr.ccs.tencentyun.com\/wolfplus\/mcbeam/ccr.ccs.tencentyun.com\/wolfplus\/mcbeam:mcbeam-v3-202011071039/g'
-  sed -i '' '/containers:/i\\      imagePullSecrets:\n      - name: qcloudregistrykey'
-
+  sed -i '' 's/mcbeam-dev:mcbeam-dev-20210809-668f94da117f906a3259f671f815dce6e169602b/mcbeam-dev:mcbeam-dev-20210810-f95008e34b07919faf9e3f6efb37b4acfe732903/g' `ls ./service/*`
+  sed -i '' 's/- name: qcloudregistrykey//g'  `ls ./service/*`
 kubectl apply -f service
 
 win:
-  sed -i '' 's/mcbeam.tencentcloudcr.com\/wolfplus\/mcbeam:mcbeam-0311/mcbeam-hub.tencentcloudcr.com\/wolfplus\/mcbeam:mcbeam-0311/g' `ls ./service/*`
-
+  sed -i '' 's/mcbeam.tencentcloudcr.com\/wolfplus\/mcbeam:mcbeam-0311/mcbeam-hub.tencentcloudcr.com\/wolfplus\/mcbeam-dev:mcbeam-dev-20210809-668f94da117f906a3259f671f815dce6e169602b/g' `ls ./service/*`
 kubectl run cockroachdb --image=ubuntu -it --rm --restart=Never --overrides='
 {
   "spec": {
