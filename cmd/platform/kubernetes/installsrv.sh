54c8dd41d1a048df03191a8f3f1d98c52bf95d08
wolfplus <wolfplus@gmail.com>
2021-09-15 12:26:39 +0800

    add

--- cmd/platform/kubernetes/installsrv.sh
+++ cmd/platform/kubernetes/installsrv.sh
@@ -6,7 +6,7 @@ if [ $MICRO_ENV == "dev" ]; then
 fi
 
 if [ $MICRO_ENV == "dev" ]; then
-  sed -i '' 's/mcbeam-dev:mcbeam-v3-20210915-c20fc8328a272b63d03ca186bdbc996efb868948/mcbeam:mcbeam-v3-20210915-c20fc8328a272b63d03ca186bdbc996efb868948/g' `ls ./service/*`
+  sed -i '' 's/mcbeam-dev:mcbeam-v3-20210915-9a3221e406719ef22e468a65e248643ae997c6a3/mcbeam:mcbeam-v3-20210915-9a3221e406719ef22e468a65e248643ae997c6a3/g' `ls ./service/*`
   sed -i '' 's/- name: qcloudregistrykey//g'  `ls ./service/*`
 kubectl apply -f service
 
